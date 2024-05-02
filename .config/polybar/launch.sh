fad2be5f730ac38558e302dc7ad1b937a661d303
HighFlip <ugoandrebc@gmail.com>
2024-05-02 17:06:06 -0400

    First commit

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sleep 2
+
 # Terminate already running bar instances
 killall -q polybar
 
@@ -10,4 +12,13 @@ foreground=$color7
 tertiary=$color2
 fourth=$color4
 
-polybar --reload main &
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+done
+
+
+# polybar --reload main &
---
+++ .config/wal/scripts/update_colorschemes.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+"$HOME/.config/dunst/launch_dunst.sh"
+
+/home/chibolo/.local/bin/pywalfox --verbose update > "$HOME/.config/wal/pywalfox_output.log" 2>&1
+
